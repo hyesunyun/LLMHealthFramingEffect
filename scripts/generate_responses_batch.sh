a389f6f1e00d2600f0751f9902a76a01552e6e66
hyesunyun <hyesunyun09@gmail.com>
2026-01-13 15:49:24 -0500

    sbatch for batch

---
+++ scripts/generate_responses_batch.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=99:00:00
+#SBATCH --job-name=batch_response
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10G
+#SBATCH --partition=177huntington
+#SBATCH -o output_%j.txt                     # Standard output file
+#SBATCH -e error_%j.txt                      # Standard error file
+
+module purge
+module load explorer anaconda3/2024.06
+
+source activate base
+source activate llm_health_framing_effect
+conda activate llm_health_framing_effect
+
+conda info
+
+export HF_HOME="/scratch/yun.hy/.cache"
+export HUGGINGFACE_HUB_CACHE="/scratch/yun.hy/.cache"
+export XDG_CACHE_HOME="/scratch/yun.hy/.cache"
+
+models=(
+  "gpt-5.1"
+  "claude_4.5_sonnet"
+)
+
+echo "Running LLM response generation"
+for model in "${models[@]}"; do
+    python3 ../code/generate_responses.py \
+        --model "$model" \
+        --input_path ../code/outputs/questions/qwen3_thinking-4B/cochrane_review_data_final_with_questions_new.json \
+        --output_path "../code/outputs/responses/$model/question_responses.json"
+done
+
+conda deactivate
