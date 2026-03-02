06f9d02bc068a40906a5303c529568e36217b4b2
hyesunyun <hyesunyun09@gmail.com>
2026-03-02 17:38:24 -0500

    edit for generate responses

--- code/generate_responses.py
+++ code/generate_responses.py
@@ -140,8 +140,27 @@ class Generator:
         results = {}
         total = len(all_questions)
 
+        # TODO: remove when not using intermediate results for check pointing.
+        intermediate_file = f"{self.output_path.removesuffix(".json")}_intermediate.json"
+        if os.path.exists(intermediate_file):
+            intermediate_results = load_json_file(intermediate_file)
+            for example in intermediate_results:
+                for q_key, q_dict in example["ModelGeneratedAnswersWithQuestions"].items():
+                    for q_type in ["positive", "negative"]:
+                        if f"{q_type}_answer" in q_dict:
+                            results[(example["review_id"], q_key, q_type)] = q_dict[f"{q_type}_answer"]
+        ############## END ##############
+
+        # have the loop start from batch index 450 (i.e., batch_num 449) so that it will continue from where it left off and avoid repeating the same batches again
+
         for batch_num, batch_start in enumerate(tqdm(range(0, total, self.batch_size), desc="Batched single-turn generation")):
             batch = all_questions[batch_start:min(batch_start + self.batch_size, total)]
+
+            # TODO: remove when not using intermediate results for check pointing.
+            if batch_num < 449:
+                continue
+            ############## END ##############
+
             messages_list = [q['messages'] for q in batch]
             responses = self.model.generate_batch_output(messages_list, max_new_tokens=self.max_new_tokens)
 
