6df7661c592ae8e3df7722abd2362427e9e85cda
hyesunyun <hyesunyun09@gmail.com>
2026-03-17 22:19:06 -0400

    update

--- scripts/generate_framing_responses.sh
+++ scripts/generate_framing_responses.sh
@@ -29,23 +29,25 @@ models=(
   # "llama3.3_instruct_70B"
   # "huatuo-7B"
   # "huatuo-8B"
-  # "huatuo-70B"
+  "huatuo-70B"
 )
 
-# echo "Running LLM response generation for default questions"
-# for model in "${models[@]}"; do
-#     python3 -u ../code/generate_responses.py \
-#         --model "$model" \
-#         --input_path ../code/outputs/questions/qwen3_thinking-4B/extracted/cochrane_review_data_final_with_questions.json \
-#         --output_path "../code/outputs/responses/$model/question_responses.json" 
-# done
-
-echo "Running LLM response generation for simplified questions"
+echo "Running LLM response generation for default questions"
 for model in "${models[@]}"; do
     python3 -u ../code/generate_responses.py \
         --model "$model" \
-        --input_path ../code/outputs/questions/qwen3_thinking-4B/simplified/cochrane_review_data_final_with_questions.json \
-        --output_path "../code/outputs/responses/$model/simplified_question_responses.json" 
+        --input_path ../code/outputs/questions/qwen3_thinking-4B/extracted/cochrane_review_data_final_with_questions.json \
+        --output_path "../code/outputs/responses/$model/question_responses.json" \
+        --batch_size 2
 done
+
+# echo "Running LLM response generation for simplified questions"
+# for model in "${models[@]}"; do
+#     python3 -u ../code/generate_responses.py \
+#         --model "$model" \
+#         --input_path ../code/outputs/questions/qwen3_thinking-4B/simplified/cochrane_review_data_final_with_questions.json \
+#         --output_path "../code/outputs/responses/$model/simplified_question_responses.json" \
+#         --batch_size 2
+# done
 
 conda deactivate
